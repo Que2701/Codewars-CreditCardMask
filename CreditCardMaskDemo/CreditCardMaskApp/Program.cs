05c0a23ba570d6cab55d21bb58e4830012f20bc4
tumelo <tumelom@evolvemed.co.za>
2020-05-06 21:00:48 +0200

    complete solution

---
+++ CreditCardMaskDemo/CreditCardMaskApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using MaskLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardMaskApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Muskify.MaskCreditCard("4556364607935616"));
+            Console.WriteLine(Muskify.MaskCreditCard("64607935616"));
+            Console.WriteLine(Muskify.MaskCreditCard("1"));
+            Console.WriteLine(Muskify.MaskCreditCard(""));
+            Console.WriteLine(Muskify.MaskCreditCard("Skippy"));
+            Console.WriteLine(Muskify.MaskCreditCard("Nananananananananananananananana Batman!"));
+
+            // Allow the user to close the application
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CreditCardMaskDemo/MaskLibrary/Muskify.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaskLibrary
+{
+    public class Muskify
+    {
+        public static string MaskCreditCard(string creditCardData)
+        {
+            if (creditCardData.Length < 4)
+                return creditCardData;
+            var maskLengthLimit = creditCardData.Length - 4;
+            var maskedData = default(string);
+            var loopController = 0;
+            creditCardData.ToList().ForEach(l =>
+            {
+                if(loopController < maskLengthLimit)
+                {
+                    creditCardData += "#";
+                }
+                loopController++;
+            });
+
+            return maskedData += creditCardData.Substring(maskLengthLimit, 4);
+        }
+    }
+}
